f95e315e81b6d32e7beaa6b071fa52f82733d0cd
cristhian vargas <cristhian.a.vargas@gmail.com>
2021-03-29 19:16:37 -0500

    javascript

---
+++ main.js
@@ -0,0 +1,23 @@
+const typedTextSpan = document.querySelector(".typed-text")
+
+const textArray = ["Full-Stack Developer" ]
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if(charIndex < textArray[textArrayIndex].length){
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    setTimeout(erase, newTextDelay)
+  }
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(type, newTextDelay + 250)
+})
